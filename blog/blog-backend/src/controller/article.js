209a84a50148323fca40fe3c5388ad60f06f3ed7
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-06-04 16:52:58 +0200

    Blog project. Backend. Setting netlify

---
+++ blog/blog-backend/src/controller/article.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const curso = (req, res) => {
+    console.log('Se ha ejecutado el endpoint probando');
+
+    return res.status(200).json([
+        {
+            curso: 'Master en React',
+            autor: 'Víctor Robles WEB',
+            url: 'victorroblesweb.es/master-react',
+        },
+        {
+            curso: 'Master en React',
+            autor: 'Víctor Robles WEB',
+            url: 'victorroblesweb.es/master-react',
+        },
+    ]);
+};
+
+module.exports = {
+    curso,
+};
--- blog/blog-backend/src/routes/article.js
+++ blog/blog-backend/src/routes/article.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const multer = require('multer');
+const ArticleController = require('../controller/article');
 
 const router = express.Router();
+
+router.get('/curso', ArticleController.curso);
 
 module.exports = router;
