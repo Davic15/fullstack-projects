526e315f6a798f210da917d59454b8e7542a4f31
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-06-13 16:11:48 +0200

    Cors problems backend

--- social-network/socialnetwork-backend/functions/api.js
+++ social-network/socialnetwork-backend/functions/api.js
@@ -17,28 +17,6 @@ const app = express();
 // Cors configurarion
 app.use(cors());
 
-/*app.use((req, res, next) => {
-    //* * all domains or specific domain
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, PATCH, DELETE'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization'
-    );
-    next();
-});*/
-/*app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});*/
-
 const userRoutes = require('./routes/user');
 const publicationRoutes = require('./routes/publication');
 const followRoutes = require('./routes/follow');
