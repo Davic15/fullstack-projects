9bbb3fd00ed11a7a4d1f544d8e6d974e2264fe1e
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-06-13 11:02:57 +0200

    CORS problems

--- social-network/socialnetwork-backend/functions/api.js
+++ social-network/socialnetwork-backend/functions/api.js
@@ -5,10 +5,6 @@ const cors = require('cors');
 require('dotenv').config();
 const connection = require('../src/database/connection');
 
-const userRoutes = require('./routes/user');
-const publicationRoutes = require('./routes/publication');
-const followRoutes = require('./routes/follow');
-
 // Welcome message to the social network
 console.log('API NODE for a social network started!.');
 
@@ -20,6 +16,32 @@ const app = express();
 
 // Cors configurarion
 app.use(cors());
+/*
+app.use((req, res, next) => {
+    //* * all domains or specific domain
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
+    next();
+});*/
+/*app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});*/
+
+const userRoutes = require('./routes/user');
+const publicationRoutes = require('./routes/publication');
+const followRoutes = require('./routes/follow');
 
 app.use(function (req, res, next) {
     const allowedHosts = [
